203cc834482f31b86222b37194de95b2e1419b83
shajjad Shagor <shajjadshagor@gmail.com>
2023-01-10 09:12:50 -0800

    design full quiz app and set answer right and wrong check and fulley check it

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { AnswerObject } from '../App';
+import { ButtonWrapper, Wrapper } from './QuestionCard.styles';
 
 type Props = {
     question: string;
@@ -12,19 +13,23 @@ type Props = {
 
 const QuestionCard: React.FC<Props> = ({ question, answers, callback, userAnswer, questionNr, totalQuestions }) =>
    (
-    <div>
+    <Wrapper>
         <p className='number'>
             Question: {questionNr} / {totalQuestions}
         </p>
         <p dangerouslySetInnerHTML={{ __html: question }}></p>
         <div>
             {answers?.map(answer => (
-                <div key={answer}>
+                <ButtonWrapper 
+                  key={answer}
+                  correct={userAnswer?.correctAnswer === answer}
+                  userClicked={userAnswer?.answer === answer}
+                  >
                     <button disabled={userAnswer ? true : false} value={answer} onClick={callback}><span dangerouslySetInnerHTML={{ __html:answer }}></span></button>
-                </div>
+                </ButtonWrapper>
             ))}
         </div>
-    </div>
+    </Wrapper>
   )
 
 export default QuestionCard;
